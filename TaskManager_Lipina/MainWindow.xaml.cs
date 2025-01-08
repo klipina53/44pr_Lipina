74b1e5737ed0fa157496003866457d85a3ab4ba2
klipina53 <klipina53@gmail.com>
2025-01-09 01:53:26 +0500

    Проработанная структура и реализованное главное окно.Lipina

--- TaskManager_Lipina/MainWindow.xaml.cs
+++ TaskManager_Lipina/MainWindow.xaml.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace TaskManager_Lipina
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
-    {
-        public MainWindow()
         {
-            InitializeComponent();
+            public static MainWindow init;
+            public MainWindow()
+            {
+                InitializeComponent();
+                init = this;
+                DataContext = new VM_Pages();
+            }
         }
-    }
+    
 }
