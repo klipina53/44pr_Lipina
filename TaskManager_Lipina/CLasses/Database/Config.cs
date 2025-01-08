e15dc853f1891945f9b1a956c021be3c23bf1b4d
klipina53 <klipina53@gmail.com>
2025-01-09 02:27:17 +0500

    Созданный класс Config. Lipina

---
+++ TaskManager_Lipina/CLasses/Database/Config.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager_Lipina.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=127.0.0.1;" +
+            "port=3306;" +
+            "uid=root;" +
+            "database=TaskManager;";
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
