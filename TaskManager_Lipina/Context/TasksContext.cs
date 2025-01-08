a8d8b2a168abe6825dc53d1318482c984e4ffe45
klipina53 <klipina53@gmail.com>
2025-01-09 03:06:59 +0500

    Разработанный контекст данных для модели задач.Lipina

---
+++ TaskManager_Lipina/Context/TasksContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using TaskManager_Lipina.Classes.Database;
+using TaskManager_Lipina.Models;
+
+namespace TaskManager_Lipina.Context
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql(Config.connection, Config.version);
+    }
+}
