a01a9fcc8fad1601045fd1cd69fb7bbbac8a5fb2
klipina53 <klipina53@gmail.com>
2025-01-09 02:09:49 +0500

    Разработанная главная страница.Lipina

---
+++ TaskManager_Lipina/View/Main.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace TaskManager_Lipina.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+            DataContext = Context;
+        }
+    }
+}
