2b6ebd9945a3cb47836cc01ced6c27f4272680f5
klipina53 <klipina53@gmail.com>
2025-01-09 03:20:02 +0500

    Разработанная модель представления главного окна.Lipina

---
+++ TaskManager_Lipina/ViewModels/VM_Pages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManager_Lipina.Classes;
+
+namespace TaskManager_Lipina.ViewModels
+{
+    public class VM_Pages : Notification
+    {
+        public VM_Tasks vm_tasks = new VM_Tasks();
+        public VM_Pages()
+        {
+            MainWindow.init.frame.Navigate(new View.Main(vm_tasks));
+        }
+        public RelayCommand OnClose
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    MainWindow.init.Close();
+                });
+            }
+        }
+    }
+}
